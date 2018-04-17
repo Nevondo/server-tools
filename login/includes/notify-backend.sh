a7885cc0d7aee949ec7d8c7286685729e05c99fb
Kevin Stenzel <mail@kevin-stenzel.de>
2018-04-17 20:39:44 +0200

    add backend login notify

--- login/includes/notify-backend.sh
+++ login/includes/notify-backend.sh
@@ -1,1 +1,7 @@
-#!/usr/bin/env bash
+KEY=%KEY%
+HOST=`hostname -f`
+USERNAME=`who -m | awk '{print $1}'`
+USERIP=`who -m | awk '{print $5}' | sed s/"("// | sed s/")."// | sed s/")"// | sed s/")."//`
+
+
+curl -k https://backend.codeink.de/api/login.php?key=$KEY&host=$HOST&username=$USERNAME&userip=$USERIP
--- login/start.sh
+++ login/start.sh
@@ -1,1 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+### Functions ###
+
+function which {
+
+    if [ -d "/etc/profile.d/notify-backend.sh" ]; then
+        echo 1
+    else
+        echo 0
+    fi
+
+}
+
+function createScript {
+
+    key=$( \
+		dialog  --title "Add Secret Authkey" \
+				--cancel-label "Cancel" \
+			    --inputbox "Type in Secret Authkey for Backend (example: 123)" 8 40 \
+		3>&1 1>&2 2>&3 3>&- \
+	)
+
+    wget https://git.codeink.de/CodeInk/server-scripts/raw/master/login/includes/notify-backend.sh
+
+	sed -i "s/%KEY%/$key/g" /etc/profile.d/notify-backend.sh
+}
+
+### Main ###
+
+if [ ! which ]; then
+    createScript
+else
+    echo "etc/profile.d/notify-backend.sh gibt es bereits!"
+fi
