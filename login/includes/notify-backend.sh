a553f57919433728f871f664b9e435491fd2c8a3
Kevin Stenzel <mail@kevin-stenzel.de>
2018-04-17 21:06:45 +0200

    Fix notify-backend

--- login/includes/notify-backend.sh
+++ login/includes/notify-backend.sh
@@ -4,4 +4,4 @@ USERNAME=`who -m | awk '{print $1}'`
 USERIP=`who -m | awk '{print $5}' | sed s/"("// | sed s/")."// | sed s/")"// | sed s/")."//`
 
 
-curl -k https://backend.codeink.de/api/login.php?key=$KEY&host=$HOST&username=$USERNAME&userip=$USERIP
+curl -k "https://backend.codeink.de/api/login.php?key=$KEY&host=$HOST&username=$USERNAME&userip=$USERIP" &> /dev/null
