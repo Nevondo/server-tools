3ed7c81f47cc1bfbdc8131472eb3f2756c64dc2c
Aiko Appeldorn <aiko@appeldorn.me>
2018-12-28 13:47:35 +0100

    o fixed password

--- installation/start.sh
+++ installation/start.sh
@@ -31,7 +31,7 @@ function cleanUp {
 
 function resetPassword {
     pw=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
-    echo "$pw" | passwd root
+    echo "root:$pw" | chpassw
     echo "********************************"
     echo "       New root password        "
     echo "$pw"
