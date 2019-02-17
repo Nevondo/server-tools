5714f76ed7e3accbaf383d248999c596d52d923a
Kevin Stenzel <mail@kevin-stenzel.de>
2019-02-17 17:23:19 +0100

    borgbackup add log

--- borgbackup/includes/backup.sh
+++ borgbackup/includes/backup.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
+exec > >(tee -i ${LOG})
+exec 2>&1
+echo "###### Starting on $(date) ######"
+
 if [ -f %BACKUP_SH_DIR%precmd.sh ]; then
     source %BACKUP_SH_DIR%precmd.sh
 fi
 
-
-# Setting this, so the repo does not need to be given on the commandline:
 export BORG_REPO=ssh://%USER%@%HOST%:22%REPO_PATH%
-
-# Setting this, so you won't be asked for your repository passphrase:
 export BORG_PASSPHRASE='%PASSPHRASE%'
-# or this to ask an external program to supply the passphrase:
 export BORG_PASSCOMMAND='pass show backup'
 
-# some helpers and error handling:
-info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
-trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
-
-info "Starting backup"
-
-# Backup the most important directories into an archive named after
-# the machine this script is currently running on:
+echo "###### Starting backup ######"
 
 borg create                         \
     --verbose                       \
@@ -47,12 +39,8 @@ borg create                         \
 
 backup_exit=$?
 
-info "Pruning repository"
+echo "###### Pruning repository ######"
 
-# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
-# archives of THIS machine. The '{hostname}-' prefix is very important to
-# limit prune's operation to this machine's archives and not apply to
-# other machines' archives also:
 
 borg prune                          \
     --list                          \
@@ -62,19 +50,26 @@ borg prune                          \
     --keep-weekly   12              \
     --keep-monthly  15              \
 
+echo "###### Finished on $(date) ######"
+
 prune_exit=$?
 
-# use highest exit code as global exit code
 global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
 
 if [ ${global_exit} -eq 1 ];
 then
-    info "Backup and/or Prune finished with a warning"
+    echo "###########################################"
+    echo "Backup and/or Prune finished with a warning"
+    echo "###########################################"
 fi
 
 if [ ${global_exit} -gt 1 ];
 then
-    info "Backup and/or Prune finished with an error"
+    echo "###########################################"
+    echo "Backup and/or Prune finished with an error"
+    echo "###########################################"
 fi
 
 exit ${global_exit}
+
+echo $LOG > last.log
