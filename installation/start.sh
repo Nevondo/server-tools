5342777061e9b7442cf8b0afa237f0809d0d817f
Kevin Stenzel <mail@kevin-stenzel.de>
2019-02-21 22:46:54 +0100

    add login-notify

--- installation/start.sh
+++ installation/start.sh
@@ -5,6 +5,7 @@ TMP="/tmp"
 CHECK_MK="https://git.codeink.de/CodeInk/server-tools/raw/master/installation/includes/check-mk-agent.deb"
 BASHRC="https://git.codeink.de/CodeInk/server-tools/raw/master/installation/includes/.bashrc"
 SSH_KEYS="https://git.codeink.de/CodeInk/server-tools/raw/master/installation/includes/authorized_keys"
+LOGIN_NOTIFY="https://git.codeink.de/CodeInk/server-tools/raw/master/installation/includes/login-notify.sh"
 VIRTUAL_HOST=false
 
 ### Functions ###
@@ -26,7 +27,7 @@ function Update {
 }
 
 function InstallPackages {
-    apt-get install aptitude molly-guard htop iftop parted tree vim curl screen screenfetch net-tools byobu xinetd -y
+    apt-get install aptitude molly-guard htop iftop parted tree vim curl screen screenfetch net-tools byobu xinetd grepcidr -y
 }
 
 function SetupMonitoring {
@@ -67,6 +68,24 @@ function SetupFsTrim {
     fi
 }
 
+function SetupLoginNotify {
+    mkdir -p /opt/scripts/login-notify/
+    rm /opt/scripts/login-notify/login-notify.sh
+    rm /etc/ssh/sshrc
+    if ! -f /opt/scripts/login-notify/config.conf ; then
+        echo -e "\n\n Telegram API Key: "
+        read telegram_api_key
+        echo -e "\n Telegram Chat ID: "
+        read telegram_chat_id
+        echo "API_KEY=$telegram_api_key" >> /opt/scripts/login-notify/config.conf
+        echo "CHAT_ID=$telegram_chat_id" >> /opt/scripts/login-notify/config.conf
+    fi
+    echo 'CONN_IP=`echo $SSH_CONNECTION | cut -d " " -f 1`' >> /etc/ssh/sshrc
+    echo '/opt/scripts/login-notify/login-notify.sh $CONN_IP' >> /etc/ssh/sshrc
+    wget $LOGIN_NOTIFY -O /opt/scripts/login-notify/login-notify.sh
+    chmod +x /opt/scripts/login-notify/login-notify.sh
+}
+
 function CleanUp {
     rm /etc/motd -f
     rm /etc/update-motd.d/* -R -f
@@ -92,5 +111,6 @@ SetupBashrc
 SetupSsh
 SetupQemuAgent
 SetupFsTrim
+SetupLoginNotify
 CleanUp
 SetRootPassword
