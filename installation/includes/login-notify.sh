2e148b85d29824f4b2cb200a4d69417df92f928a
Kevin Stenzel <mail@kevin-stenzel.de>
2019-02-26 21:54:03 +0100

    Installscript fix login

--- installation/includes/login-notify.sh
+++ installation/includes/login-notify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 APIKEY=$(cat /opt/codeink/.apikey)
-curl -4 -s "https://backend.codeink.de/api/index.php?push_ssh_log&apikey=$APIKEY&username=$USER&userip=$1"
+curl -4 -s "https://backend.codeink.de/api/index.php?push_ssh_log&apikey=$APIKEY&username=$USER&userip=$1" >> /dev/null
