a6caf3104a7fc6983f59974d561c5b740c12f3af
Acie <thomas@wittenwiler.org>
2019-02-26 21:11:25 +0100

    Update Link

--- installation/includes/login-notify.sh
+++ installation/includes/login-notify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apikey=$(cat /opt/codeink/.apikey)
-curl -4 -s "https://backend.codeink.de/api/index.php?apikey=$apikey&username=$USER&userip=$1&push_ssh_log"
+curl -4 -s "https://backend.codeink.de/api/index.php?push_ssh_log&apikey=$apikey&username=$USER&userip=$1"
