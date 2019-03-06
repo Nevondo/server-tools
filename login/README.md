fd9a5053fb6be19c4bb4b2b6e8e50a045d3c58d1
Aiko Appeldorn <aap@codeink.de>
2019-03-06 13:28:46 +0100

    - removed login

--- login/README.md
+++
@@ -1,3 +0,0 @@
-# CodeInk - Backend Notify
-
-`curl https://git.codeink.de/CodeInk/server-tools/raw/master/login/start.sh | bash`
--- login/includes/notify-backend.sh
+++
@@ -1,7 +0,0 @@
-KEY=%KEY%
-HOST=`hostname -f`
-USERNAME=`who -m | awk '{print $1}'`
-USERIP=`who -m | awk '{print $5}' | sed s/"("// | sed s/")."// | sed s/")"// | sed s/")."//`
-
-
-curl -k "https://backend.codeink.de/api/login.php?key=$KEY&host=$HOST&username=$USERNAME&userip=$USERIP" &> /dev/null
