d915ce96bd2ec5981fcca33ce10be24b5488c95c
Kevin Stenzel <mail@kevin-stenzel.de>
2020-01-06 20:05:49 +0100

    edit borgmatic

--- borgmatic/setup_client.sh
+++ borgmatic/setup_client.sh
@@ -102,7 +102,7 @@ function setupClient {
     greenMessage "Store the following public key on the backup server:\n"
     cyanMessage "$(cat /root/.ssh/id_ed25519.pub) \n"
     greenMessage "Then create repo:\n"
-    cyanMessage "borg init $HOSTNAME@backup01.nevondo.com:repo"
+    cyanMessage "borg init --encryption=repokey-blake2 $HOSTNAME@backup01.nevondo.com:repo"
 
 }
 
